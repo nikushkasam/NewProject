a021a612532f974ff48451e4432a680c658e658c
nikushkasam <nikita.samelyuk@github.com>
2020-03-07 15:42:59 +0200

    Dev commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String develop;
         System.out.println("Hi");
         System.out.println("Hi");
         System.out.println("Hi");
