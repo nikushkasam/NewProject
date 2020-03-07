5e90a313703551aec52c1d1d75d9e94441ea09a1
nikushkasam <nikita.samelyuk@github.com>
2020-03-07 15:06:07 +0200

    New Changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int a = 10;
-        System.out.println(a);
-        int b = 10;
-        System.out.println(b);
-        int c = 10;
-        System.out.println(c);
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
     }
 }
